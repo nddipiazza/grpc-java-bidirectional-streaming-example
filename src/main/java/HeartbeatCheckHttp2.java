a36d955d94b1a3e0c9bfbbb54e1317eb93fc24e4
Nicholas DiPiazza <nicholas.dipiazza@gmail.com>
2018-10-25 20:40:52 -0500

    fix it

--- src/main/java/HeartbeatCheckHttp2.java
+++ src/main/java/HeartbeatCheckHttp2.java
@@ -1,21 +1,19 @@
-import org.apache.commons.io.IOUtils;
-
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 public class HeartbeatCheckHttp2 {
     public static void main(String [] args) throws Exception {
         for (int i=0; i<50000; ++i) {
             Thread.sleep(400L);
-            Socket socket = new Socket();
-            try {
-                socket.connect(new InetSocketAddress("localhost", 50000));
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                IOUtils.closeQuietly(socket);
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress("localhost", 50000), 5000);
+                System.out.println("Connected");
+            } catch (Exception ex) {
+
+                System.out.println("Not connected");
             }
+            Thread.sleep(TimeUnit.SECONDS.toMillis(2));
         }
     }
 }
